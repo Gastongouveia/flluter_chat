26d038125215b22258b1e1c7a2d5cbb9b261ded0
Gaston <gaston.gouveia@informata.com.br>
2021-08-31 18:49:31 -0300

    feat: formulário de cadastro e login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
-import 'package:chat/screens/chat_screen.dart';
+import 'package:chat/pages/auth_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -14,7 +17,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ChatScreen(),
+      home: AuthPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
